bad2c10ca510e3ada73195385bf51e197e7cb133
dervis.kopuz <derviskopuz@gmail.com>
2021-03-31 22:38:53 +0300

    tree done

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -11,72 +11,41 @@ Date: <06/04/2021> */
 #include <list>
 #include <vector>
 #include <math.h>
+#include <algorithm>
 
 #include "data_structs.h"
 
 using namespace std;
 
-class Core {
-  public:
-  string letter;
-  int value;
-
-  Core(string letter, int value);
-};
-
-Core::Core (string letter1, int value1) {
-  letter = letter1;
-  value = value1;
-}
-
 class Node {
   //string letter;
   //int value;
-  vector<Core>* cores;
-  int verticeNum;
-  bool visited;
+  
+  
 
   public:
-    Node(Core core, int verticeNum);
+    Node(string letter, int value, int verticeNum);
     Node();
-    int getVerticeNum();
-    //int getValue();
-    //string getLetter();
-    bool getVisited();
-    void setVisited(bool value);
-    Core getCore(int coreNumber);
-    void addCore(Core core);
-};
 
-Node::Node (Core core, int verticeNum1) {
-  cores = new vector<Core>;
-  cores->push_back(core);
+    vector<string>* letters;
+    vector<int>* values;
+    int verticeNum;
+    bool visited;
+    };
+
+Node::Node (string letter, int value, int verticeNum1) {
+  letters = new vector<string>;
+  letters->push_back(letter);
+  values = new vector<int>;
+  values->push_back(value);
   verticeNum = verticeNum1;
   visited = false;
 }
 
 Node::Node() {
-  
-}
-
-int Node::getVerticeNum() {
-  return verticeNum;
-}
-
-bool Node::getVisited() {
-  return visited;
-}
-
-void Node::setVisited(bool value) {
-  visited = value;
-}
-
-Core Node::getCore(int coreNum) {
-  return cores->at(coreNum);
-}
-
-void Node::addCore(Core core) {
-  cores->push_back(core);
+  letters = new vector<string>;
+  values = new vector<int>;
+  visited = false;
 }
 
 class Graph {
@@ -84,28 +53,28 @@ class Graph {
   //vector<Node>* adj;
 
   public:
-    Node* vertex;
     Graph(Node* vertex);
-    void addEdge(Node* src, Node* dest);
-    void BFS(Node startVertex);
+    void addEdge(Node src, Node* dest);
+    void BFS(Node startVertex,string operand1, string operand2, string result);
 
+    Node* vertex;
     vector<Node>* adj;
 };
 
 Graph::Graph(Node* vertex1) {
   vertex = vertex1;
-  int verticeNum = vertex1->getVerticeNum();
+  int verticeNum = vertex1->verticeNum;
   //adj boyutunu dinamik hale getir
-  adj = new vector<Node>[1000];
+  adj = new vector<Node>[1000000];
 }
 
-void Graph::addEdge(Node* parent, Node* child) {
-  adj[parent->getVerticeNum()].push_back(*child);
+void Graph::addEdge(Node parent, Node* child) {
+  adj[parent.verticeNum].push_back(*child);
 }
 
-void Graph::BFS(Node node) {
+void Graph::BFS(Node node, string operand1, string operand2, string result) {
   
-  int verticeNum = node.getVerticeNum();
+  int verticeNum = node.verticeNum;
   //list<bool>* visited = new list<bool>[50];
   //for(int i = 0; i < 50; i++) {
     //visited[i] = false;
@@ -114,7 +83,7 @@ void Graph::BFS(Node node) {
   list<Node> queue;
 
   //mark current node visited and enqueue
-  node.setVisited(true);
+  node.visited = true;
   queue.push_back(node);
 
   list<Node>::iterator i;
@@ -122,15 +91,37 @@ void Graph::BFS(Node node) {
   int adjCounter = 0;
   while(!queue.empty()) {
     node = queue.front();
-    cout << "Vertice Num: " << node.getVerticeNum() << endl;
+    //cout << "Vertice Num: " << node.verticeNum << endl;
+    //cout << "Letters Num: " << node.letters << endl;
     queue.pop_front();
 
     int adjSize = adj[adjCounter].size();
     //vector<Node> adj = new vector<int>[100];
     for (int i = 0; i != adjSize; ++i) {
       auto test = adj[adjCounter].at(i);
-      if(!adj[adjCounter].at(i).getVisited()) {
-        adj[adjCounter].at(i).setVisited(true);
+      if(!adj[adjCounter].at(i).visited) {
+        Node currentNode = adj[adjCounter].at(i);
+        currentNode.visited = true;
+        cout << currentNode.verticeNum << endl;
+
+        //aynı sayı kontrolü
+        auto valueIterator = unique(currentNode.values->begin(), currentNode.values->end());
+        bool hasDuplicates = !(valueIterator == currentNode.values->end());
+        if (hasDuplicates) {
+          continue;
+        }
+
+        //not 0 conditions
+        int operand1Length = operand1.length();
+        for (int i = 0; i < operand1Length - 1; i++) {
+          string letter = string(1, operand1.at(i));
+          for (int k = 0; k < currentNode.letters->size(); k++) {
+            if (letter == currentNode.letters->at(k))
+              currentNode.values->at(k) == 0
+          }
+        }
+
+
         queue.push_back(adj[adjCounter].at(i));
       }
     }
@@ -140,35 +131,48 @@ void Graph::BFS(Node node) {
   }
 }
 
-bool checkSummationRuleStart(Core operand1, Core operand2, Core result) {
+bool checkZeroConitions(string operand1, string operand2, string result) {
+
+}
+
+bool checkSummationRuleStart(int operand1, int operand2, int result) {
   int carry = 0;
-  if ((operand1.value + operand2.value) >= 10) {
+  if ((operand1 + operand2) >= 10) {
     carry = 1;
   }
 
-  if ((operand1.value + operand2.value) == (result.value + 10 * carry)) {
+  if ((operand1 + operand2) == (result + 10 * carry)) {
     return true;
   }
   return false;
 }
 
-bool checkSummationRule(Core operand1, Core operand2, Core result, int carry1) {
+bool checkSummationRule(int operand1, int operand2, int result, int carry1) {
   int carry2 = 0;
-  if ((operand1.value + operand2.value + carry1) >= 10) {
+  if ((operand1 + operand2 + carry1) >= 10) {
     carry2 = 1;
   }
 
-  if ((operand1.value + operand2.value + carry1) == (result.value + 10 * carry2)) {
+  if ((operand1 + operand2 + carry1) == (result + 10 * carry2)) {
     return true;
   }
   return false;
 }
 
-bool checkSummationRuleEnd(Core operand, int carry) {
-  if(operand.value == carry) {
+bool checkSummationRuleEnd(int operand, int carry) {
+  if(operand == carry) {
     return true;
   }
   return false;
+}
+
+Node* cloneNode(Node parent) {
+  Node* newNode = new Node();
+  *newNode->letters = *parent.letters;
+  *newNode->values = *parent.values;
+  newNode->visited = false;
+
+  return newNode;
 }
 
 int main(int argc, char* argv[])
@@ -183,6 +187,7 @@ int main(int argc, char* argv[])
   ifstream file(filename);
   string input;
 
+/*
   Core* newCore = new Core("a",1);
   Node* newNode0 = new Node(*newCore,0);
   Node* newNode1 = new Node(*newCore,1);
@@ -192,6 +197,7 @@ int main(int argc, char* argv[])
   Node* newNode5 = new Node(*newCore,5);
   Node* newNode6 = new Node(*newCore,6);
   Node* newNode7 = new Node(*newCore,7);
+
 
   Graph graph(newNode0);
   graph.addEdge(newNode0,newNode1);
@@ -205,7 +211,7 @@ int main(int argc, char* argv[])
   cout << "Breadth First Traversal ";
 	cout << "(starting from vertex 0) \n";
 	graph.BFS(*newNode0);
-  
+  */
 
   if(!file)
   {
@@ -214,7 +220,7 @@ int main(int argc, char* argv[])
   }
 
   vector<int> numbers = {0,1,2,3,4,5,6,7,8,9};
-  vector<string> letters = {"T","W","O","F","U","R"};
+  list<string> letters = {"T","W","O","F","U","R"};
   //get inputs line by line
   /*
   while(getline(file, input)) {
@@ -243,28 +249,51 @@ int main(int argc, char* argv[])
   int nodeCount =pow(numbers.size(), letters.size());
 
   //Core* newCore = new Core("start",1);
-  //Node* startNode = new Node(newCore, 0);
-  //Graph graph(startNode);
+  Node* startNode = new Node("start", 0, 0);
+  Graph graph(startNode);
   //graph.adj[0].push_back(*startNode);
-  int verticeCounterLetter = 0;
-  int verticeCounterNumber = 0;
-  int verticeCounterCopy = 0;
+  int verticeCounterNumber = 1;
+  int layerCount = 0;
+  int adjCounter = 0;
 
   //start node a eklemeler
   for( string letter : letters) {
-    for(int i = 0; i < 10; i++) {
-      for( int number : numbers) {
-        Core* newCore = new Core(letter, number); 
-        //Node* newNode = new Node(newCore, verticeCounterNumber);
-        //graph.addEdge(graph.vertex, newNode); //Add a new node with every value to parent
-        verticeCounterNumber++;
-
-      }
-      verticeCounterCopy++;
+    for( int number : numbers) {
+      Node* newNode = new Node(letter, number, verticeCounterNumber);
+      graph.addEdge(*graph.vertex, newNode); //Add a new node with every value to parent
+      verticeCounterNumber++;
     }
-    verticeCounterLetter++;
-
+    break;
   }
+
+  //graph.BFS(*startNode);
+
+  layerCount++;
+  letters.pop_front();
+
+  for( string letter : letters) {
+    for(int k = 0; k<layerCount; k++) { //graph.adj[adjCounter].size()
+      for(int i = 0; i < 10; i++) {
+        for( int number : numbers) {
+          //cout << graph.adj[adjCounter].at(i).letters->back() <<endl;
+          Node* newNode = cloneNode(graph.adj[adjCounter].at(i));
+          newNode->verticeNum = verticeCounterNumber;
+          newNode->letters->push_back(letter);
+          newNode->values->push_back(number);
+          graph.addEdge(graph.adj[adjCounter].at(i), newNode); //Add a new node with every value to parent
+          verticeCounterNumber++;
+
+        }
+      }
+      adjCounter++;
+    }
+    if (layerCount == 0) {
+      layerCount = 1;
+    }
+    layerCount *= 10;
+  }
+
+  graph.BFS(*startNode, operand1, operand2, result);
 
   cout << "hello world" << endl;
   
